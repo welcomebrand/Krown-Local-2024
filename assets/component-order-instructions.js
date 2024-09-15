93eae6fa83ec806192f9c47f9b02fd02deb4166f
James Young <james@welcomebrand.co.uk>
2024-09-15 19:25:57 +0100

    initial commit

---
+++ assets/component-order-instructions.js
@@ -0,0 +1,17 @@
+if ( document.getElementById('cartSpecialInstructions') ) {
+
+  KROWN.saveToSessionStorage = () => {
+    const specialInstructions = document.getElementById('cartSpecialInstructions').value;
+    sessionStorage.setItem('cartSpecialInstructions', specialInstructions);
+  }
+  document.getElementById('cartSpecialInstructions').addEventListener('input', KROWN.saveToSessionStorage);
+
+  KROWN.loadFromSessionStorage = () => {
+    const savedInstructions = sessionStorage.getItem('cartSpecialInstructions');
+      if ( savedInstructions ) {
+        document.getElementById('cartSpecialInstructions').value = savedInstructions;
+      }
+  }
+  document.addEventListener('DOMContentLoaded', KROWN.loadFromSessionStorage);
+
+}
